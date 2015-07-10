e994b902207fe634eb8d7c108ddb47557b41af2c
akDeveloper <andreas.kollaros@gmail.com>
2015-07-11 01:49:01 +0300

    fix php 5.4 compatible

---
+++ src/compatible.php
@@ -0,0 +1,15 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+if (!defined('JSON_ERROR_RECURSION')) {
+    define('JSON_ERROR_RECURSION', 6);
+}
+
+if (!defined('JSON_ERROR_INF_OR_NAN')) {
+    define('JSON_ERROR_INF_OR_NAN', 7);
+}
+
+if (!defined('JSON_ERROR_UNSUPPORTED_TYPE')) {
+    define('JSON_ERROR_UNSUPPORTED_TYPE', 8);
+}
