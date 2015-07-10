8b86a4dd2e58f09b25e4f343a21f6577a1951233
akDeveloper <andreas.kollaros@gmail.com>
2015-07-11 01:19:22 +0300

    add compatible file for defines.

--- src/Json/JsonError.php
+++ src/Json/JsonError.php
@@ -4,10 +4,6 @@
 
 namespace Json;
 
-if (!defined('JSON_ERROR_RECURSION')) define('JSON_ERROR_RECURSION', 6);
-if (!defined('JSON_ERROR_INF_OR_NAN')) define('JSON_ERROR_INF_OR_NAN', 7);
-if (!defined('JSON_ERROR_UNSUPPORTED_TYPE')) define('JSON_ERROR_UNSUPPORTED_TYPE', 8);
-
 class JsonError
 {
     private $error;
